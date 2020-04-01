c819f6c35d86088d82a389d93101deb555a7d424
inoublii <inoublii@gmail.com>
2020-04-01 05:00:27 +0100

    User

--- models/user.py
+++ models/user.py
@@ -14,13 +14,7 @@ class User(BaseModel, Base):
         last_name: last name
     """
     __tablename__ = 'users'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
