067464d21f23c2baa19cae3840d60a79f61a5d31
inoublii <inoublii@gmail.com>
2020-04-02 14:58:42 +0100

    review property

--- models/review.py
+++ models/review.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This is the review class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 import os
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """This is the class for Review
     Attributes:
         place_id: place id
