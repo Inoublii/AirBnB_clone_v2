fd82a08cca48a3d9afed657641dff36ee44ecec1
inoublii <inoublii@gmail.com>
2020-04-12 22:24:18 +0100

    fx

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,9 +1,9 @@
 # Redo the task #0 but by using Puppet:
 exec { 'execute':
-  command => 'sudo apt-get -y update;
-              sudo apt-get -y install nginx;
-              sudo mkdir -p /data/web_static/releases/test/;
+  command => 'sudo apt-get update -y;
+              sudo apt-get install nginx -y;
               sudo mkdir -p /data/web_static/shared/;
+              sudo mkdir -p /data/web_static/releases/test/;
               sudo touch /data/web_static/releases/test/index.html;
               sudo echo "Hello Holberton School" > /data/web_static/releases/test/index.html;
               sudo ln -sf /data/web_static/releases/test /data/web_static/current;
