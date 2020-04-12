1bc27fe24407fe0a1a91cb982b8b514d1f57eee3
inoublii <inoublii@gmail.com>
2020-04-12 20:54:22 +0100

    f

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,6 @@ from fabric.api import *
 import os
 from datetime import datetime
 import sys
-env.user = 'ubuntu'
 env.hosts = ["35.227.78.112	", "54.81.167.121"]
 
 
@@ -32,18 +31,17 @@ def do_deploy(archive_path):
     if not archive_path:
         return(False)
     try:
+        new_comp = archive_path.split("/")[-1]
+        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
         put(archive_path, "/tmp/")
-        deploy_file = archive_path.split("/")[1].split(".")[0]
-        file_path = file_deploy.split(".")[0]
-        path = archive_path.split("/")[-1]
-        deploy_path = "/data/web_static/releases/{}".format(deploy_file)
-        run("mkdir -p {}".format(deploy_path))
-        run("tar -xzf /tmp/{} - C {}".format(path, deploy_path))
-        run("rm -rf /tmp/{}".format(path))
-        run("mv {}/web_static/* {}/".format(deploy_path, deploy_path))
-        run("rm -rf {}/web_static".format(deploy_path))
-        run("rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(deploy_path))
-        return(True)
-    except BaseException:
-        return(False)
+        run("sudo mkdir -p {}".format(new_folder))
+        run("sudo tar -xzf /tmp/{} -C {}".
+            format(new_comp, new_folder))
+        run("sudo rm /tmp/{}".format(new_comp))
+        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("sudo rm -rf {}/web_static".format(new_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        return True
+    except:
+        return False
