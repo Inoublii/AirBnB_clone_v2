458d01eaff251f38913298758b657e71990d8b29
inoublii <inoublii@gmail.com>
2020-04-12 18:01:52 +0100

    fx

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ def do_pack():
 
     datenow = strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-    creat = local("tar -cvzf versions/web_static_{}.tgz web_static".format(datenw))
+    creat = local("tar -cvzf versions/web_static_{}.tgz web_static".format(datenow))
     if creat.succeeded:
         return ("versions/web_static_{}".format(datenow))
     else:
