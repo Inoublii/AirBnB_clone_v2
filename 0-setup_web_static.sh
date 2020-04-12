7ea93a68c463b63f5e6bcbba1dcdda558fe3e353
inoublii <inoublii@gmail.com>
2020-04-12 17:51:25 +0100

    fx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+# script to set up web servers for web_static deployment
 sudo apt-get install update
 sudo apt-get install -y nginx
+
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
-sudo chown ubuntu:ubuntu -R /data/
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-FILE=/etc/nginx/sites-available/default
-REDIRECT_STRING="location /hbnb_static {\n alias /data/web_static/current; \n}\n"
-sudo sed -i "39i $REDIRECT_STRING" $FILE
-sudo service nginx restart
 
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '35 i\   \tlocation /hbnb_static {\n\talias /data/web_static/current;\n}' /etc/nginx/sites-available/default
+
+sudo service nginx restart
