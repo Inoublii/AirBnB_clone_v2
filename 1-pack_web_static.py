5985e34a330706f6262f4d49bf613056124d4764
inoublii <inoublii@gmail.com>
2020-04-12 18:00:50 +0100

    fx

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,15 +2,16 @@
 '''Fabric script
 '''
 from time import strftime
-from datetime import datetime
+from fabric.api import *
 
 
 def do_pack():
     """function"""
 
+    datenow = strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-    creat = local("tar -cvzf versions/web_static_{}.tgz web_static".format(strftime("%Y%m%d%H%M%S")))
+    creat = local("tar -cvzf versions/web_static_{}.tgz web_static".format(datenw))
     if creat.succeeded:
-        return ("versions/web_static_{}".format(strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}".format(datenow))
     else:
         return None
