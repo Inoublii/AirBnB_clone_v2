05eb37abb9f1802e5dad884a66185c0408fc2aa3
inoublii <inoublii@gmail.com>
2020-04-12 21:46:11 +0100

    f

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,53 +2,58 @@
 '''Fabric script that generates a .tgz archive from the contents
    of Airbnb_clone_v2
 '''
-from datetime import datetime
+from time import strftime
 from fabric.api import *
-import sys
+from fabric.operations import run, put, sudo
 import os
-env.user = 'ubuntu'
+import os.path
+from datetime import datetime
 env.hosts = ["35.227.78.112	", "54.81.167.121"]
 
 
 def do_pack():
-    """function"""
-    n = datetime.now()
-    name = "web_static_{}{}{}{}{}{}.tgz".format(n.year, n.month,
-                                                n.day, n.hour,
-                                                n.minute, n.second)
-    local("mkdir -p versions")
-    if name is None:
-        return(None)
-    return(local("tar -cvzf {} web_static".format(name)))
+    """fabric script that
+    compresses files"""
 
+    datenow = strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    creat = local(
+        "tar -cvzf versions/\web_static_{}.tgz web_static".format(datenow))
+    size = os.stat("versions/web_static_{}.tgz".format(datenow)).st_size
+    if creat.succeeded:
+        print("web_static packed:\
+versions/web_static_{}.tgz -> {}Bytes".format(datenow, size))
+    else:
+        return None
 
 
 def do_deploy(archive_path):
-    '''func deploy'''
-    if not archive_path:
-        return(False)
+    ''' def deploy '''
+    if (os.path.isfile(archive_path) is False):
+        return False
+
     try:
+        new_comp = archive_path.split("/")[-1]
+        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
         put(archive_path, "/tmp/")
-        deploy_file = archive_path.split("/")[1].split(".")[0]
-        file_path = file_deploy.split(".")[0]
-        path = archive_path.split("/")[-1]
-        deploy_path = "/data/web_static/releases/{}".format(deploy_file)
-        run("mkdir -p {}".format(deploy_path))
-        run("tar -xzf /tmp/{} - C {}".format(path, deploy_path))
-        run("rm -rf /tmp/{}".format(path))
-        run("mv {}/web_static/* {}/".format(deploy_path, deploy_path))
-        run("rm -rf {}/web_static".format(deploy_path))
-        run("rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(deploy_path))
-        return(True)
-    except BaseException:
-        return(False)
+        run("sudo mkdir -p {}".format(new_folder))
+        run("sudo tar -xzf /tmp/{} -C {}".
+            format(new_comp, new_folder))
+        run("sudo rm /tmp/{}".format(new_comp))
+        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("sudo rm -rf {}/web_static".format(new_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        return True
+    except:
+        return False
 
 
 def deploy():
-    '''func'''
+    ''' def deploy '''
     try:
-        path = do_pack()
-    except BaseException:
-        return(False)
-    do_deploy(path)
+        addr = do_pack()
+        take = do_deploy(addr)
+        return take
+    except:
+        return False
