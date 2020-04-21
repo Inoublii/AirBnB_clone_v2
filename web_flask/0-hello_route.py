93f7103d28a6c77463b8c81c78c463d992efc35c
root <root@HI.localdomain>
2020-04-21 21:59:46 +0100

    flast env

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-	"""Hello"""
-	return("Hello HBNB!")
+    """Hello"""
+    return("Hello HBNB!")
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
